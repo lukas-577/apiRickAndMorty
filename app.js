f1efb683e18132e4eedd77ee0e11386e80776d2d
lukas <lukas_skyry_14@hotmail.cl>
2023-10-30 12:04:18 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,73 @@
+
+const cargaData = async (page) => {
+    try {
+        const res = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+        console.log(res)
+
+        const datosJson = await res.json();
+        console.log(datosJson)
+
+        const resulados = datosJson.results
+
+        console.log(resulados)
+
+        pintaCard(resulados)
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const pintaCard = (personaje) => {
+
+    const container = document.querySelector('.card-container')
+    container.innerHTML = ''
+
+    const row = document.createElement('div')
+    row.classList.add('row')
+
+    personaje.map(item => {
+        const col = document.createElement('div');
+        col.classList.add('col-md-4')
+
+        col.innerHTML = `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${item.image}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text">status: ${item.status}</p>
+                        <p class="card-text">especie: ${item.species}</p>
+                        <p class="card-text"><small class="text-body-secondary">${item.location.name}</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        `
+        row.appendChild(col)
+    })
+    container.appendChild(row)
+
+
+
+}
+
+
+function navigate(page) {
+
+
+    console.log(page)
+
+    cargaData(page)
+}
+
+
+
+
+cargaData(1)
